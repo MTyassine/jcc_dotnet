d67557247c10701ba7735a6516568a8abc6b7e9c
MT Yassine Foudhaili <MT Yassine Foudhaili>
2016-11-29 17:53:00 +0100

    first commit

--- JCC/Models/Cinema.vb
+++
@@ -1,9 +0,0 @@
-﻿Using System;
-Using System.Data.Entity;
-Namespace JCC.Models
-    Public Class Cinema{
-    Public int id {Get;Set;}
-    Public String Nom{Get; Set;}
-    Public String Localisation{Get; Set;}
-    Public virtual ICollection<Projection> Projections { Get; Set; }
-            }
--- JCC/Models/ticket.vb
+++
@@ -1,9 +0,0 @@
-﻿Using System;
-Using System.Data.Entity;
- Namespace JCC.Models;
-Public Class ticket{
-    Public int id{Get; Set;}
-            Public virtual Projection{Get;Set;}
-
-
-    }
